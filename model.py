5375ec3bdb2ce91aefb758fd1e3c31bf37515914
liujianfei11 <119658596+liujianfei11@users.noreply.github.com>
2025-12-12 15:07:38 +0800

    Update model.py

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ from torchvision import models
 
 def build_model(num_classes=2):
     model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
-    in_features = model.classifier[1].in_features ##分类头在 model.classifier
+    in_features = model.classifier[1].in_features
     model.classifier[1] = nn.Linear(in_features, num_classes)
     return model
+
